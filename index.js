2a3772e74f5afb2aabf3de9ad616187a53c5253e
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-02 22:01:41 +0600

    inital db connection

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (res, req) => {
+  res.send("Ema john app is running");
+});
+
+app.listen(port, () => {
+  console.log("Ema-john server is listenign to ", port);
+});
+
+//Database
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.mj0nqa8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Db connect");
+  client.close();
+});
